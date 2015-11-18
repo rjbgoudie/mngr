3daa5febba633463fba9946bb1c722779bd2bd49
rjbgoudie <rjbgoudie@users.noreply.github.com>
2015-11-18 15:03:57 +0000

    Load in job and arm number into R

--- R/init.R
+++ R/init.R
@@ -1,38 +1,18 @@
 #' @export
-rjb_init <- function(){
+startup <- function(){
   if (!interactive()){
     args <- commandArgs(TRUE)
-    eval.parent(parse(text = paste("jid <- '", args[[1]], "'")))
+    eval.parent(parse(text = paste(".job <- ", args[[1]])))
     cat("Time:", format(Sys.time(), "%a %b %d %H:%M:%S %Y"), "\n")
-    cat("wd:", getwd(), "\n")
-    cat("jobID:", jobID, "\n")
+    cat("Working directory:", getwd(), "\n")
+    cat("Job:", .job, "\n")
     if (args[[2]] != "undefined"){
-      eval.parent(parse(text = paste("jobName <- ", shQuote(as.character(args[[2]])))))
+      eval.parent(parse(text = paste(".arm <- ", args[[2]])))
     }
     else {
-      eval.parent(parse(text = paste("jobName <- 'Untitled'")))
+      eval.parent(parse(text = paste(".arm <- 1")))
     }
-    cat("jobName:", jobName, "\n")
-    if (args[[3]] != "undefined"){
-      eval.parent(parse(text = paste("taskID <- ", args[[3]])))
-    }
-    else {
-      eval.parent(parse(text = paste("taskID <- 1")))
-    }
-    cat("taskID:", taskID, "\n")
-    if (length(args) > 3) {
-      for (i in 4:length(args)){
-        ai <- args[[i]]
-        if (isTRUE(grepl("=", ai))){
-          eval.parent(parse(text = paste(ai)))
-        }
-        else {
-          settrue <- " <- T"
-          eval.parent(parse(text = paste(ai, settrue)))
-        }
-      }
-      cat("More arguments loaded..\n")
-    }
+    cat("Arm:", .arm, "\n")
   }
   else {
     warning("Not in BATCH mode - not loading arguments\n")
