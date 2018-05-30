rjbgoudie/mngr

commit ffb72f3442594735168fe89398a364e9bf7fd41a

Fix read_rds_merge
Rather than trying to reconstruct the arm ids from scratch, use a new
strategy: just obtain the arm ids of the prerequisite tasks, and use
these instead

R/init.R
@@ -36,7 +36,8 @@ startup <- function(){
   }
   mngrfile <- find_mngrfile(getwd())
   source(mngrfile)
-  arms <- arms_all(.task)
+  task_obj <- task_get(task, exists = TRUE)
+  arms <- task_obj$arms_to_invoke(.task)
   if (.arm > nrow(arms)){
     stop("Can't find arm ", .arm, "; there are only ", nrow(arms), " arms")
   }

R/arm.R
@@ -12,15 +12,6 @@ arms_factorial <- function(...){
   assign("arms_list", arms_list, envir = task_env)
 }
 
-#' All arms of a task
-#'
-#' @param task A task name
-#' @return A list of all arms for the task
-arms_all <- function(task){
-  task_obj <- task_get(task, exists = TRUE)
-  task_obj$arms_to_invoke()
-}
-
 #' Unique name for arm
 #'
 #' Create a character name for a particular arm
@@ -28,27 +19,20 @@ arms_all <- function(task){
 #' NOTE THIS SEEMS TO DUPLICATE taskarm_name??
 #'
 #' @return A character vector of length 1, the arm_name
-arm_name <- function(task, splitting = FALSE){
+arm_id <- function(task, splitting = FALSE){
   task_obj <- task_get(task, exists = TRUE)
   task_obj$arm_ids(splitting = splitting)[.arm]
 }
 
-#' The arm names prior to merging
+#' Arm IDs for the prerequisite tasks
 #'
-#' @return A list of arm names
-arm_name_merge <- function(){
-  arms <- arms_all(.task)
-
-  arm <- arms[[.arm]]
-  o <- order(names(arm))
-  arm <- arm[o]
-  arml <- lapply(arm, as.character)
-  grid <- do.call("expand_grid", arml)
-  apply(grid, 1, function(arm){
-    arm_values <- sapply(arm, paste, collapse = ",")
-    arm_names <- names(arm)
-    paste(arm_names, arm_values, sep = "--",  collapse = "__")
-  })
+#' @param task A task name
+#' @return
+#' A list of arm ids of the current arm of the supplied task
+prereq_arm_ids <- function(task){
+  task_obj <- task_get(task, exists = TRUE)
+  prereq_ids <- task_obj$prereq_ids(id = "arm", throttle = FALSE)
+  prereq_ids[[.arm]]
 }
 
 #' Merge arms
@@ -85,7 +69,7 @@ arm_name_merge <- function(){
 #' @return A list, each component of which corresponds to a unmerged arm
 #' @export
 read_rds_merge <- function(...){
-  all_arms <- arm_name_merge()
+  all_arms <- prereq_arm_ids(.task)
   all_paths <- sapply(all_arms, function(x){
     rds_file(..., arm = x)
   })

R/files.R
@@ -9,7 +9,7 @@
 #' @return A character vector of length 1, the file path
 #' @export
 file_path <- function(file,
-                      arm = arm_name(.task, splitting = splitting),
+                      arm = arm_id(.task, splitting = splitting),
                       directory = "etc",
                       extension = "",
                       splitting = FALSE){

R/task.R
@@ -230,7 +230,7 @@ Task <- setRefClass(
       job_ids()[indicies]
     },
 
-    which_jobs_involve = function(match){
+    which_arms_involve = function(match, id){
       "For each row of a given data_frame arm values (match), identify which
        arms in THIS task involve the arm values in that match row. "
 
@@ -255,16 +255,23 @@ Task <- setRefClass(
       # For each row of the match data_frame, which rows in this are involved
       indicies_list <- apply(involved, 1, which)
 
+      id_fun <- if (id == "job"){
+        .self$job_ids
+      } else if (id == "arm") {
+        .self$arm_ids
+      }
+
       # Convert these indicies to job_ids
       lapply(indicies_list, function(index){
-        as.list(job_ids()[index])
+        as.list(id_fun()[index])
       })
     },
 
     build_jobs = function(){
       arms_local <- arms_to_invoke()
       jobs_df <- bind_cols(arms_local,
-                           tibble(prereq_job_ids = prereq_job_ids()))
+                           tibble(prereq_job_ids = prereq_ids(id = "job",
+                                                              throttle = TRUE)))
 
       jobs_df <- jobs_df %>%
         mutate(task_name = name,
@@ -323,7 +330,7 @@ Task <- setRefClass(
       lapply(tasks, function(task) task$invoke(debug = debug))
     },
 
-    prereq_job_ids = function(){
+    prereq_ids = function(id, throttle){
       "Return a list, each component corresponds to an arm of this task"
 
       arms_local <- arms_to_invoke()
@@ -339,24 +346,28 @@ Task <- setRefClass(
         # A list each component of which corresponds to a task.
         # Each component contains a component corresponding to each arm of this
         # task
-        prereq_jobids_by_prereq <- lapply(prerequisities, function(task){
+        prereq_ids_by_prereq <- lapply(prerequisities, function(task){
           if (task$is_dummy()){
             # transpose so that we get the form
             # arms, each component is the list of prereqs
-            purrr::transpose(task$prereq_job_ids())
+            purrr::transpose(task$prereq_ids(id = id, throttle = throttle))
           } else {
-            task$which_jobs_involve(arms_local)
+            task$which_arms_involve(arms_local, id = id)
           }
         })
         # Flip so that prerequisities tasks are nested within arms
-        prereq_jobids_by_arm <- purrr::transpose(prereq_jobids_by_prereq)
+        prereq_ids_by_arm <- purrr::transpose(prereq_ids_by_prereq)
 
         # Then for each arm, unlist to remove task level
-        result <- lapply(prereq_jobids_by_arm, unlist, recursive = FALSE)
+        result <- lapply(prereq_ids_by_arm, unlist, recursive = FALSE)
       }
 
-      # Add (throttle)th previous arm, so as to throttle
-      purrr::map2(result, throttle_job_ids(), append_unless_na)
+      if (throttle){
+        # Add (throttle)th previous arm, so as to throttle
+        purrr::map2(result, throttle_job_ids(), append_unless_na)
+      } else {
+        result
+      }
     },
 
     add_merge = function(new_merge){

commit e76abbe3f3c55fba8b9653386bc5c18090c61c9f

Fixup getting arms in init function
Corrects error introduced by ffb72f3442594735168fe89398a364e9bf7fd41a

R/init.R
@@ -36,8 +36,8 @@ startup <- function(){
   }
   mngrfile <- find_mngrfile(getwd())
   source(mngrfile)
-  task_obj <- task_get(task, exists = TRUE)
-  arms <- task_obj$arms_to_invoke(.task)
+  task_obj <- task_get(.task, exists = TRUE)
+  arms <- task_obj$arms_to_invoke()
   if (.arm > nrow(arms)){
     stop("Can't find arm ", .arm, "; there are only ", nrow(arms), " arms")
   }